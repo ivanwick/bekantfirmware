4774e0ae836240d2140cebfcf24cafcb45030f1e
Ivan Wick <ivanwick+github@gmail.com>
2021-01-08 21:30:52 -0800

    Document oscillator/timer settings

--- bekantfirmware.X/system.c
+++ bekantfirmware.X/system.c
@@ -10,8 +10,11 @@ void ConfigureOscillator(void)
     OSCCONbits.IRCF = 0b1111; // 16 MHz HF
     OSCCONbits.SCS = 0b11; // Internal oscillator block
 
+    // Watchdog timer has an independent clock source
     WDTCONbits.WDTPS = 0b00101; // 1:1024 (Interval 32ms)
     WDTCONbits.SWDTEN = 1; // WDT turned on
+    // 32 ms @ Fosc/4 (instruction timer) = 
+    // 32 ms * 4 Mhz = 128_000 instructions before watchdog reset
     
     OPTION_REGbits.nWPUEN = 1; // !WPUEN Weak Pull ups disabled
     OPTION_REGbits.INTEDG = 1; // INTEDG Interrupt on rising edge of INT pin
@@ -19,8 +22,13 @@ void ConfigureOscillator(void)
     OPTION_REGbits.TMR0SE = 1; // TMR0SE Increment on high-to-low transition on T0CKI pin
     OPTION_REGbits.PSA = 0; // PSA Prescaler is assigned to the Timer0 module
     OPTION_REGbits.PS = 0b010; // 010 Prescaler 1:8
-    
-    // TODO document what this is in real time units
+
+    // Timer2 clock input is Fosc/4 (instruction clock)
+    // System Fosc: 16 Mhz
+    // Instruction clock: Fosc / 4 = 4 Mhz
+    // 4 Mhz / 49 period / 10 postscaler = ~8163 Hz
+    //   0.0001225 sec
+    //   122.5 usec
     PR2bits.PR2 = 0x31; // Timer2 period
     
     T2CONbits.T2OUTPS = 0b1001; // 1:10 Postscaler
