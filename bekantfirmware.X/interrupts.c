eebbc48935a59f2ff82a34c169d900bd87e8b5ce
Ivan <ivan@t61>
2021-01-27 22:40:30 -0800

    Set up BCTRL LIN frame timer and interrupt

--- bekantfirmware.X/interrupts.c
+++ bekantfirmware.X/interrupts.c
@@ -4,11 +4,15 @@
 #include <stdbool.h>
 
 #include "lin/lin_d.h"
+#include "bekant/bctrl.h"
 
 void __interrupt() isr(void)
 {
     if (PIR1bits.RCIF) {
         lin_txrx_daemon();
         PIR1bits.RCIF = 0;
+    } else if (PIR3bits.TMR4IF) {
+        bctrl_timer();
+        PIR3bits.TMR4IF = false;
     }
 }
