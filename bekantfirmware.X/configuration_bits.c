293de93968bde33282286245a9943758549c7706
Ivan Wick <ivanwick+github@gmail.com>
2021-01-28 20:56:37 -0800

    Configuration bits copied from OEM dump.hex except MCLRE
    MCLR/VPP pin function is MCLR so it can be reset from the PICKit 2

--- bekantfirmware.X/configuration_bits.c
+++ bekantfirmware.X/configuration_bits.c
@@ -1,33 +1,30 @@
-/******************************************************************************/
-/* Main Files to Include                                                      */
-/******************************************************************************/
 
-#if defined(__XC)
-    #include <xc.h>         /* XC8 General Include File */
-#elif defined(HI_TECH_C)
-    #include <htc.h>        /* HiTech General Include File */
-#endif
+// PIC16LF1938 Configuration Bit Settings
 
-/******************************************************************************/
-/* Configuration Bits                                                         */
-/*                                                                            */
-/* Refer to your Hi-Tech User Manual in the PICC installation directory       */
-/* /doc folder for more information on filling in configuration bits.         */
-/* In addition, configuration bit mnemonics can be found in your              */
-/* PICC\version\include\<processor name>.h file for your device.  The XC8     */
-/* compiler contains documentation on the configuration bit macros within     */
-/* the compiler installation /docs folder in a file called                    */
-/* pic18_chipinfo.html.                                                       */
-/*                                                                            */
-/* For additional information about what the hardware configurations mean in  */
-/* terms of device operation, refer to the device datasheet.                  */
-/*                                                                            */
-/* A feature of MPLAB X is the 'Generate Source Code to Output' utility in    */
-/* the Configuration Bits window.  Under Window > PIC Memory Views >          */
-/* Configuration Bits, a user controllable configuration bits window is       */
-/* available to Generate Configuration Bits source code which the user can    */
-/* paste into this project.                                                   */
-/*                                                                            */
-/******************************************************************************/
+// 'C' source line config statements
 
-/* TODO Fill in your config bits here.  Use the configuration bits generator/ */
+// CONFIG1
+#pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config WDTE = ON        // Watchdog Timer Enable (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#pragma config IESO = OFF       // Internal/External Switchover (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+// CONFIG2
+#pragma config WRT = ALL        // Flash Memory Self-Write Protection (000h to 3FFFh write protected, no addresses may be modified by EECON control)
+#pragma config PLLEN = OFF      // PLL Enable (4x PLL disabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = HI        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), high trip point selected.)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+
